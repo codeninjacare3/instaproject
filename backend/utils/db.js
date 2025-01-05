9bbfd3a8cc60ecd11717c6c0868e30db7972025c
Aashima <dev6.gainsboro@gmail.com>
2025-01-05 17:29:40 +0530

    mongo URL testing

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -3,7 +3,12 @@ import mongoose from "mongoose";
 const connectionstring = "";
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000, // Reduce timeout
+            socketTimeoutMS: 45000,        // Socket timeout
+        });
         console.log('mongodb connected successfully.');
     } catch (error) {
         console.log(error);
